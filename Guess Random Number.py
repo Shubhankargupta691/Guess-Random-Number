ff7de249d0df2a24036b04da965c98ddcfcf76ed
Shubhankar Gupta <115340551+Shubhankargupta691@users.noreply.github.com>
2023-04-18 21:30:16 +0530

    Update Guess Random Number.py

--- Guess Random Number.py
+++ Guess Random Number.py
@@ -1,10 +1,12 @@
 import random
-number = random.randint(1,5)
-for i in range(0,5):
-    user = int(input("Guess the number"))
-    if user  == number:
-        print('hurray')
-        print(f"your guesss number is right {number}")
+
+number = random.randint(1, 100)
+while True:
+    guess = int(input("Guess the number"))
+    if guess == number:
+        print("Congratulations! You guessed the number.")
         break
-if user != number:
-    print(f"your guesss number is incorrect {number}")
+    elif guess < number:
+        print("Too low. Guess again.")
+    else:
+        print("Too high. Guess again.")
